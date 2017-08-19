fd450b155340ee4393c3e482ff46e4a74cf49a2d
Tyriek Barker <tyriekbarker@gmail.com>
2017-08-19 04:21:40 +0000

    Done.

--- lib/display_rainbow.rb
+++ lib/display_rainbow.rb
@@ -1,1 +1,4 @@
 # Write your #display_rainbow method here
+def display_rainbow(array_of_colors)
+  puts "#{array_of_colors[0][0].upcase}: #{array_of_colors[0]}, #{array_of_colors[1][0].upcase}: #{array_of_colors[1]}, #{array_of_colors[2][0].upcase}: #{array_of_colors[2]}, #{array_of_colors[3][0].upcase}: #{array_of_colors[3]}, #{array_of_colors[4][0].upcase}: #{array_of_colors[4]}, #{array_of_colors[5][0].upcase}: #{array_of_colors[5]}, #{array_of_colors[6][0].upcase}: #{array_of_colors[6]}"
+end
